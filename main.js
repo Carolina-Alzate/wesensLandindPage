5fbe3c20ae39b00d8a09b6582354d3aeb2fa5f4e
Carolina-Alzate <carolinaalzate12@gmail.com>
2021-09-01 18:58:23 -0500

    letters

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bg = document.getElementById("bg");
 let tree = document.getElementById("tree");
 let sheet = document.getElementById("sheet");
+let tittle = document.getElementById("tittle");
 let wild = document.getElementById("wild");
 
 window.addEventListener("scroll", function () {
@@ -9,6 +10,7 @@ window.addEventListener("scroll", function () {
   bg.style.top = value * 0.2 + "px";
   tree.style.left = -value * 0.2 + "px";
   sheet.style.top = value * 1 + "px";
-  wild.style.marginTop = -value * 2 + "px";
-  wild.style.marginLeft = value * 2 + "px";
+  tittle.style.marginTop = value * 3 + "px";
+  wild.style.marginLeft = -value * 2 + "px";
+  wild.style.marginTop = -value * 3 + "px";
 });
